f1753668067ed6426a6d1accaf5b0bb5bc371c3b
jasminedraycott <jasminedraycott@yahoo.ca>
2023-01-12 13:27:39 -0500

    Camera somewhat works
    Need to fix scaling, speed, angle, and the weird jump to the top thing it does when you start moving

---
+++ Lumi/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector3 velocity = Vector3.one;
+    public Transform cameraT;
+    public Transform target;
+    public Vector3 offset;
+
+    void Start()
+    {
+        offset = cameraT.position - target.position;
+    }
+
+    void Awake(){
+        cameraT = transform;
+    }
+
+    void LateUpdate()
+    {
+        Follow();
+    }
+
+    public void Follow(){
+        Vector3 moveTo = target.position + (target.rotation * offset);
+        Vector3 currentPos = Vector3.SmoothDamp(cameraT.position, moveTo, ref velocity, 0.3f);
+        cameraT.position = currentPos;
+        cameraT.LookAt(target, target.up);
+    }
+}
